a5241e210c99970b4283584833155da05d0c5152
ArtmeLyksa <artemlyksa@gmail.com>
2016-04-16 12:59:23 +0300

    1

--- UsingJSON_Demo.playground/Contents.swift
+++ UsingJSON_Demo.playground/Contents.swift
@@ -1,1 +1,120 @@
 import UIKit
+class VideoGame: CustomStringConvertible {
+    var name = ""
+    var genre = ""
+    var rating = 0
+    var synopsis = ""
+    
+    var description: String {
+        
+        return "name: \(name), genre: \(genre), rating: \(rating), \(synopsis)"
+    }
+}
+
+class GameParser: NSObject {
+    var xmlParser: NSXMLParser?
+    var games:[VideoGame] = []
+    var xmlText = ""
+    var currentGame:VideoGame?
+    
+    init(withXML xml: String) {
+        if let data = xml.dataUsingEncoding(NSUTF8StringEncoding) {
+            xmlParser = NSXMLParser(data: data)
+        }
+    }
+    
+    func parse() -> [VideoGame] {
+        xmlParser?.delegate = self
+        xmlParser?.parse()
+        return games
+    }
+}
+
+extension GameParser: NSXMLParserDelegate {
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        xmlText = ""
+        if elementName == "video_game" {
+            currentGame = VideoGame()
+        }
+    }
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "name" {
+            currentGame?.name = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            
+        }
+        if elementName == "genre" {
+            currentGame?.genre = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            
+        }
+        if elementName == "synopsis" {
+            currentGame?.synopsis = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            
+        }
+        if elementName == "rating" {
+            if let rating = Int(xmlText) {
+                currentGame?.rating = rating
+            }
+        }
+        if elementName == "video_game" {
+            if let game = currentGame {
+                games.append(game)
+            }
+        }
+    }
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        xmlText += string
+    }
+}
+
+do {
+
+    if let xmlUrl = NSBundle.mainBundle().URLForResource("video_games", withExtension: "xml") {
+        let xml = try String(contentsOfURL: xmlUrl)
+        let gameParser = GameParser(withXML: xml)
+        let games = gameParser.parse()
+        var topLevel : [AnyObject] = []
+        
+        for game in games {
+            
+            var gameDict: [String:AnyObject] = [:]
+            gameDict["name"] = game.name
+            gameDict["genre"] = game.genre
+            gameDict["rating"] = NSNumber(integer:game.rating)
+            gameDict["synopsis"] = game.synopsis
+            topLevel.append(gameDict)
+        }
+//        let jsonData = try NSJSONSerialization.dataWithJSONObject(topLevel, options: .PrettyPrinted)
+        let fileManager = NSFileManager.defaultManager()
+        let url = try fileManager.URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+        let jsonUrl = url.URLByAppendingPathComponent("video_games.json")
+//        print(jsonUrl)
+//        jsonData.writeToURL(jsonUrl, atomically: true)
+        let jsonReadData = NSData(contentsOfURL: jsonUrl)
+        var videoGames: [VideoGame] = []
+        if let jsonReadData = jsonReadData {
+            let parsedGames = try NSJSONSerialization.JSONObjectWithData(jsonReadData, options: .MutableContainers)
+            let games = parsedGames as! [[String:AnyObject]]
+            for game in games {
+
+                guard let gameName = game["name"] as? String, genre = game["genre"] as? String,
+                    rating = game["rating"] as? Int, synopsis = game["synopsis"] as? String
+                    else { continue }
+                
+                let videoGame = VideoGame()
+                videoGame.name = gameName;
+                videoGame.genre = genre;
+                videoGame.rating = rating;
+                videoGame.synopsis = synopsis;
+
+                videoGames.append(videoGame)
+            }
+        }
+
+        for game in videoGames {
+            print(game)
+        }
+
+    }
+} catch {
+    print(error)
+}
